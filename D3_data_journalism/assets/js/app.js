578f62f2c4789e1bbfc66b7cb78e3ef197dd2013
Amir Vazquez <j.amir.vazquez@gmai.com>
2021-03-23 20:42:56 -0700

    Update app.js
    initializing variables

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,26 @@
 // @TODO: YOUR CODE HERE!
+//Initialize variables for svg
+
+var svgWidth = 800;
+var svgHeight = 450;
+
+// Initialize margin varible
+var margin ={
+    top: 20,
+    bottom: 60,
+    right: 40,
+    left: 60
+};
+
+var Width = svgWidth - margin.left - margin.right;
+var Height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+  
